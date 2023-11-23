eb9db377dd92f3a6ed2ad6508546ff751591df02
Elijah Walker North Jones <elijahwalkernorthjones@gmail.com>
2023-11-23 17:05:36 -0500

    worked more on the styling and added a couple cloud animations

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const chakras = document.querySelectorAll('.chakras');
 chakras.forEach((chakra) => {
   chakra.addEventListener('click', (e) => {
     e.preventDefault();
+    // console.log('chakra clicked:', chakra)
+
     const isEnlarged = chakra.classList.contains('largeChakra');
+    // console.log('Is enlarged?', isEnlarged);
 
     // Check if any chakra is currently enlarged
     const enlargedChakra = document.querySelector('.largeChakra');
+    // console.log('Enlarged chakra:', enlargedChakra);
 
     if (enlargedChakra) {
+      // console.log('Removing largeChakra class from another chakra:', enlargedChakra);
       // If a chakra is already enlarged and it's not the one that was clicked, remove the "largeChakra" class and reset its transform
       if (enlargedChakra !== chakra) {
         enlargedChakra.classList.remove('largeChakra');
@@ -17,8 +22,10 @@ chakras.forEach((chakra) => {
 
     // Toggle the "largeChakra" class and transform of the clicked chakra
     if (isEnlarged) {
+      // console.log('Removing largeChakra class from the clicked chakra:', chakra);
       chakra.classList.remove('largeChakra');
     } else {
+      // console.log('Adding largeChakra class to the clicked chakra:', chakra);
       chakra.classList.add('largeChakra');
     }
   });
